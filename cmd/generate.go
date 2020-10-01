395557c1c29de86767d81c0b487bd19e8dde4289
Paul <423357+toothbrush@users.noreply.github.com>
2020-10-01 16:10:46 +0930

    generate.go: Completions! Lean on ListTOTPs() to prompt shell.

--- cmd/generate.go
+++ cmd/generate.go
@@ -15,15 +15,30 @@ import (
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/pquerna/otp/totp"
 	"github.com/spf13/cobra"
+	"github.com/toothbrush/clitotp-go/files"
 )
 
 var generateCmd = &cobra.Command{
 	Use:   "generate KEYNAME",
 	Short: "Generate a TOTP",
-	Args:  cobra.ExactArgs(1),
 	Long: `
 Pass a filename of something in your $HOME/.totp directory.
 `,
+	Args: cobra.ExactValidArgs(1),
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+
+		if len(args) != 0 {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+
+		totps, err := files.ListTOTPs()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			os.Exit(1)
+		}
+
+		return totps, cobra.ShellCompDirectiveNoFileComp
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		home, err := homedir.Dir()
 		if err != nil {
